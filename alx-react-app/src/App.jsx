f1da6e77a4cfc553879fec1ce130f769bc62904a
ritjiishaku <ritjiishaku@gmail.com>
2025-02-11 10:05:05 +0100

    Added UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,7 +7,6 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,8 +17,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      {/* <UserProfile /> */}
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
-// src/components/UserProfile.jsx
-const UserProfile = ({ name, age, bio }) => {
-  return (
-    <div className="profile-card">
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p><strong>Bio:</strong> {bio}</p>
-    </div>
-  );
-};
+// // src/components/UserProfile.jsx
+// const UserProfile = ({ name, age, bio }) => {
+//   return (
+//     <div className="profile-card">
+//       <h2>{name}</h2>
+//       <p>Age: {age}</p>
+//       <p><strong>Bio:</strong> {bio}</p>
+//     </div>
+//   );
+// };
 
-export default UserProfile;
+// export default UserProfile;
 
