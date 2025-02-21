bbd32879f9d2518b54a964c1525c292cc13fb728
ritjiishaku <ritjiishaku@gmail.com>
2025-02-21 12:44:44 +0100

    alx-react-app-props

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,1 +1,19 @@
-const userContext = React.createContext()
+import React, { createContext, useContext, useState } from "react";
+
+const userContext = React.createContext();
+
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+  
+    return (
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
+    );
+  };
+
+  export const useUser = () => {
+    return useContext(UserContext);
+  }
+
+  export default UserContext;
