124740dd27a1f38478036256284f557cd381b12a
ritjiishaku <ritjiishaku@gmail.com>
2025-02-21 12:56:38 +0100

    alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,15 @@
-import ProfilePage from './ProfilePage';
+import React from "react";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={{ user: userData }}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
