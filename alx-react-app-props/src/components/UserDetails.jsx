85ea25fd3c87dfc82561d623f50ba23d8736d752
ritjiishaku <ritjiishaku@gmail.com>
2025-02-21 14:13:29 +0100

    alx-react-app-props

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,10 +1,20 @@
-function UserDetails({ userData }) {
+import React, { useContext } from "react";
+import UserContext from "./components/UserContext";
+
+const UserDetails = () => {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <p>No user data available.</p>;
+  }
+
   return (
     <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
+      <h2>User Details</h2>
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
     </div>
   );
-}
+};
 
 export default UserDetails;
