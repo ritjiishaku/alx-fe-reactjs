3ee58395717075c5d21df519d933a8f9e6970c5f
ritjiishaku <ritjiishaku@gmail.com>
2025-02-22 17:43:01 +0100

    yes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
