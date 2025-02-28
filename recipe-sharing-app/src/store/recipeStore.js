ae1afb44889ab3deaf2358d59547da543a291f05
ritjiishaku <ritjiishaku@gmail.com>
2025-02-28 12:26:23 +0100

    recipe-sharing-app

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -19,21 +19,7 @@ const useRecipeStore = create(set => ({
     ),
   })),
 
-  deleteRecipe: (id) => set(state => ({
-    recipes: state.recipes.filter(recipe => recipe.id !== id),
-    filteredRecipes: state.filteredRecipes.filter(recipe => recipe.id !== id),
-  })),
-
-  updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-    filteredRecipes: state.filteredRecipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-  })),
-
-  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes }),
+  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes })
 }));
 
 export default useRecipeStore;
