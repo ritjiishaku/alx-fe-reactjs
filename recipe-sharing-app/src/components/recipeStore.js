264b5078ebb464baed5f5237bbed23e4288a6dd4
ritjiishaku <ritjiishaku@gmail.com>
2025-02-28 12:27:40 +0100

    recipe-sharing-app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,14 +1,25 @@
-// src/store/recipeStore.js
 import { create } from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
-  updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe)
+  searchTerm: '',
+  filteredRecipes: [],
+  
+  setSearchTerm: (term) => set(state => {
+    const filtered = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(term.toLowerCase())
+    );
+    return { searchTerm: term, filteredRecipes: filtered };
+  }),
+  
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe],
+    filteredRecipes: [...state.recipes, newRecipe].filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
   })),
-  setRecipes: (recipes) => set({ recipes }),
+
+  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes })
 }));
 
 export default useRecipeStore;
