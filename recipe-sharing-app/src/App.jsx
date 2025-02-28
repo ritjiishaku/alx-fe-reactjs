f96cbfce4abdb3c99343fa41502c5818386d1cef
ritjiishaku <ritjiishaku@gmail.com>
2025-02-28 12:24:07 +0100

    recipe-sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,15 @@
-// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import DeleteRecipeButton from './components/DeleteRecipeButton';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
     <Router>
       <div>
         <h1>Recipe Sharing Application</h1>
+        <SearchBar /> {/* Add search functionality */}
         <AddRecipeForm />
         <RecipeList />
         <Routes>
