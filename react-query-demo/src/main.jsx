dc34a0da189ef5ded1b5cceacbfb50c327613195
ritjiishaku <ritjiishaku@gmail.com>
2025-03-06 14:25:19 +0100

    Initial commit with React Query setup

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
