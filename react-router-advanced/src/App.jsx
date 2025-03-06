47449d42bf66c205347a98b58db4a0a027090251
ritjiishaku <ritjiishaku@gmail.com>
2025-03-06 16:30:56 +0100

    Implemented advanced routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,19 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile/*" element={<Profile />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/:postId" element={<BlogPost />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route 
+          path="/profile/*" 
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
