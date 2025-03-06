fca788ba65a8ed1a8ffe6618e3a693379a5f1be0
ritjiishaku <ritjiishaku@gmail.com>
2025-03-06 16:43:32 +0100

    Implemented advanced routing

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
 import { Navigate } from "react-router-dom";
-
-const isAuthenticated = () => {
-  return localStorage.getItem("auth") === "true"; // Simulated authentication check
-};
+import { useAuth } from "../context/AuthContext"; // Ensure correct path
 
 const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/" />;
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
